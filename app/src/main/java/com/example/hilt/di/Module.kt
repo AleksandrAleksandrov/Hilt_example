b55959fb54c5f1a15a5fc007b7be68c9d09eba5f
Aleksandrov Aleksandr <AleksandrAleksandrovUA@gmail.com>
2022-11-07 05:44:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hilt/di/Module.kt
@@ -0,0 +1,10 @@
+package com.example.hilt.di
+
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@dagger.Module
+@InstallIn(SingletonComponent::class)
+class Module {
+
+}
